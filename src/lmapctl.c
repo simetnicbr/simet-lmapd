simetnicbr/simet-lmapd

commit d99c5541196323f1e8b9e9b412022c2dd1d6115b

lmapctl: report: prepare to accept structured output from tasks
Prepare "lmapctl report" to handle structured output from tasks.  Do
note that all task output being processed would have to be of the same
type right now, due to the lack of output type autodetection.

Add a CLI interface (-i <type>) to select the task output type, as type
autodetection is not implemented yet.  Until type autodetection is
implemented, leave the default to CSV for compatibility.

CEPTRO.br-issue: MDC-449

src/lmapctl.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <strings.h>
 #include <assert.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -71,6 +72,7 @@ static struct
 };
 
 static struct lmapd *lmapd = NULL;
+static int task_input_ft = LMAP_FT_CSV;
 
 static void
 atexit_cb(void)
@@ -104,6 +106,7 @@ usage(FILE *f)
 #ifdef WITH_XML
 	    "\t-x use xml format when generating output (default)\n"
 #endif
+	    "\t-i [json|xml] use structured input for reports\n"
 	    "\t-h show brief usage information and exit\n",
 	    LMAPD_LMAPCTL);
 }
@@ -389,7 +392,7 @@ report_cmd(int argc, char *argv[])
      */
 
     lmapd_workspace_init(lmapd);
-    if (lmapd_workspace_read_results(lmapd) == -1) {
+    if (lmapd_workspace_read_results(lmapd, task_input_ft)) {
 	return 1;
     }
 
@@ -669,7 +672,7 @@ main(int argc, char *argv[])
     char *queue_path = NULL;
     char *run_path = NULL;
 
-    while ((opt = getopt(argc, argv, "q:c:r:C:hjx")) != -1) {
+    while ((opt = getopt(argc, argv, "q:c:r:C:i:hjx")) != -1) {
 	switch (opt) {
 	case 'q':
 	    queue_path = optarg;
@@ -701,6 +704,29 @@ main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	    }
 	    break;
+	case 'i':
+	    /* FIXME: whitespace trim this, for "-i foo" as a single arg */
+	    if (!strncasecmp(optarg, "json", 5)) {
+#ifdef WITH_JSON
+		task_input_ft = LMAP_FT_JSON;
+		break;
+#else
+		lmap_err("JSON IO engine unavailable");
+		exit(EXIT_FAILURE);
+#endif
+	    } else if (!strncasecmp(optarg, "xml", 4)) {
+#ifdef WITH_XML
+		task_input_ft = LMAP_FT_XML;
+		break;
+#else
+		lmap_err("XML IO engine unavailable");
+		exit(EXIT_FAILURE);
+#endif
+	    } else {
+		lmap_err("unknown structured input format for reports: %s", optarg);
+		exit(EXIT_FAILURE);
+	    }
+
 	default:
 	    usage(stderr);
 	    exit(EXIT_FAILURE);

src/workspace.c
@@ -35,6 +35,7 @@
 #include "lmapd.h"
 #include "utils.h"
 #include "csv.h"
+#include "lmap-io.h"
 #include "workspace.h"
 
 static const char delimiter = ';';
@@ -653,13 +654,15 @@ read_result(int fd)
 }
 
 int
-lmapd_workspace_read_results(struct lmapd *lmapd)
+lmapd_workspace_read_results(struct lmapd *lmapd, const int filetype)
 {
     char *p;
     struct dirent *dp;
     DIR *dfd;
     struct table *tab;
     struct result *res;
+
+    int rc = 0;
 
     dfd = opendir(".");
     if (!dfd) {
@@ -676,32 +679,46 @@ lmapd_workspace_read_results(struct lmapd *lmapd)
 	    continue;
 	}
 
+	/* note: security issue if len("meta") != len("data") */
 	if (!strcmp(p, ".meta")) {
-	    int mfd, dfd;
+	    int mfd, datafd;
 	    mfd = open(dp->d_name, O_RDONLY);
 	    if (mfd == -1) {
 		lmap_err("failed to open meta file '%s': %s",
 			 dp->d_name, strerror(errno));
+		rc = -1;
 		continue;
 	    }
 	    strcpy(p, ".data");
-	    dfd = open(dp->d_name, O_RDONLY);
-	    if (dfd == -1) {
+	    datafd = open(dp->d_name, O_RDONLY);
+	    if (datafd == -1) {
 		lmap_err("failed to open data file '%s': %s",
 			 dp->d_name, strerror(errno));
 		(void) close(mfd);
+		rc = -1;
 		continue;
 	    }
 	    res = read_result(mfd);
 	    if (res) {
 		lmap_add_result(lmapd->lmap, res);
-		tab = read_table(dfd);
-		if (tab) {
-		    lmap_result_add_table(res, tab);
+		if (filetype == LMAP_FT_CSV) {
+		    tab = read_table(datafd);
+		    if (tab) {
+			lmap_result_add_table(res, tab);
+		    } else {
+			rc = -1;
+		    }
+		} else {
+		    if (lmap_io_parse_task_results_fd(datafd, filetype, res)) {
+			lmap_err("failed to read data file '%s'", dp->d_name);
+			rc = -1;
+		    }
 		}
 	    }
+	    (void) close(mfd);
+	    (void) close(datafd);
 	}
     }
     (void) closedir(dfd);
-    return 0;
+    return rc;
 }

src/json-io.c
@@ -1731,6 +1731,13 @@ lmap_json_parse_report_string(struct lmap *lmap, const char *string)
     return parse_string(lmap, string, &parse_report_doc);
 }
 
+int
+lmap_json_parse_task_results_fd(int fd, struct result *result)
+{
+    /* FIXME: implement this */
+    lmap_err("parsing of structured task output in JSON not implemented yet!");
+    return -1;
+}
 
 /*
  * JSON output I/O and rendering/serializing

src/json-io.h
@@ -34,6 +34,7 @@ extern int lmap_json_parse_state_string(struct lmap *lmap, const char *string);
 
 extern int lmap_json_parse_report_file(struct lmap *lmap, const char *file);
 extern int lmap_json_parse_report_string(struct lmap *lmap, const char *string);
+extern int lmap_json_parse_task_results_fd(int fd, struct result *result);
 
 extern char * lmap_json_render_config(struct lmap *lmap);
 extern char * lmap_json_render_state(struct lmap *lmap);

src/lmap-io.c
@@ -15,6 +15,8 @@
  * along with lmapd. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <unistd.h>
+
 #include "lmap.h"
 #include "lmap-io.h"
 
@@ -84,6 +86,19 @@ const char *lmap_io_engine_ext(void)
 	return en[lmap_io_engine];
 
     return "";
+}
+
+int lmap_io_parse_task_results_fd(int fd, int filetype, struct result *result)
+{
+#ifdef WITH_XML
+    if (filetype == LMAP_FT_XML)
+	return lmap_xml_parse_task_results_fd(fd, result);
+#endif
+#ifdef WITH_JSON
+    if (filetype == LMAP_FT_JSON)
+	return lmap_json_parse_task_results_fd(fd, result);
+#endif
+    return -1;
 }
 
 #ifdef WITH_XML

src/lmap-io.h
@@ -27,6 +27,12 @@ enum lmap_io_engine {
     LMAP_IO_MAX
 };
 
+/* filetypes */
+#define LMAP_FT_UNKNOWN 0
+#define LMAP_FT_XML     1
+#define LMAP_FT_JSON    2
+#define LMAP_FT_CSV     3
+
 /* change active engine, call at any point */
 extern int lmap_io_set_engine(enum lmap_io_engine engine);
 
@@ -41,5 +47,8 @@ extern int lmap_io_parse_state_path(struct lmap *lmap, const char *path);
 extern char *lmap_io_render_config(struct lmap *lmap);
 extern char *lmap_io_render_state(struct lmap *lmap);
 extern char *lmap_io_render_report(struct lmap *lmap);
+
+/* for queue IO */
+extern int lmap_io_parse_task_results_fd(int fd, int filetype, struct result *result);
 
 #endif

src/workspace.h
@@ -35,6 +35,6 @@ extern int lmapd_workspace_action_open_meta(struct schedule *schedule, struct ac
 extern int lmapd_workspace_action_meta_add_start(struct schedule *schedule, struct action *action, struct task *task);
 extern int lmapd_workspace_action_meta_add_end(struct schedule *schedule, struct action *action);
 
-extern int lmapd_workspace_read_results(struct lmapd *lmapd);
+extern int lmapd_workspace_read_results(struct lmapd *lmapd, const int filetype);
 
 #endif

src/xml-io.c
@@ -2681,4 +2681,12 @@ exit:
     return report;
 }
 
+int
+lmap_xml_parse_task_results_fd(int fd, struct result *result)
+{
+    /* FIXME: implement this */
+    lmap_err("parsing of structured task output in XML not implemented yet!");
+    return -1;
+}
+
 #endif /* ifdef WITH_XML */

commit a2c7a785b87b9c87b61cc013b404039f90513665

lmapctl: report: do not add incomplete/invalid report data
When generating a lmap report, ensure that only task output (output of
an lmap schedule's action) that has been fully loaded without parsing
and validation errors is accepted.

Empty output should be accepted and added to the report, as long as it
is in the proper format for "empty output", and its metadata is correct.

"lmapctl report" will only return an error status if it hits a general
problem (bad config data for example): it will return a success exit
status regardless of how much invalid input data it skips.

Fixes: commit d99c5541196323f1e8b9e9b412022c2dd1d6115b
       (lmapctl: report: prepare to accept structured output from tasks)

CEPTRO.br-issue: MDC-1017
CEPTRO.br-issue: MDC-449

src/lmapctl.c
@@ -393,7 +393,7 @@ report_cmd(int argc, char *argv[])
 
     lmapd_workspace_init(lmapd);
     if (lmapd_workspace_read_results(lmapd, task_input_ft)) {
-	return 1;
+	return 0;
     }
 
     report = lmap_io_render_report(lmapd->lmap);

src/workspace.c
@@ -662,7 +662,9 @@ lmapd_workspace_read_results(struct lmapd *lmapd, const int filetype)
     struct table *tab;
     struct result *res;
 
-    int rc = 0;
+    int had_errors = 0;
+    int valid_report = 0;
+    int err;
 
     dfd = opendir(".");
     if (!dfd) {
@@ -686,7 +688,7 @@ lmapd_workspace_read_results(struct lmapd *lmapd, const int filetype)
 	    if (mfd == -1) {
 		lmap_err("failed to open meta file '%s': %s",
 			 dp->d_name, strerror(errno));
-		rc = -1;
+		had_errors = 1;
 		continue;
 	    }
 	    strcpy(p, ".data");
@@ -695,24 +697,34 @@ lmapd_workspace_read_results(struct lmapd *lmapd, const int filetype)
 		lmap_err("failed to open data file '%s': %s",
 			 dp->d_name, strerror(errno));
 		(void) close(mfd);
-		rc = -1;
+		had_errors = 1;
 		continue;
 	    }
 	    res = read_result(mfd);
 	    if (res) {
-		lmap_add_result(lmapd->lmap, res);
+		err = 1;
+
 		if (filetype == LMAP_FT_CSV) {
 		    tab = read_table(datafd);
 		    if (tab) {
 			lmap_result_add_table(res, tab);
-		    } else {
-			rc = -1;
+			err = 0;
 		    }
 		} else {
-		    if (lmap_io_parse_task_results_fd(datafd, filetype, res)) {
-			lmap_err("failed to read data file '%s'", dp->d_name);
-			rc = -1;
+		    if (!lmap_io_parse_task_results_fd(datafd, filetype, res)) {
+			err = 0;
 		    }
+		}
+
+		if (!err && lmap_result_valid(lmapd->lmap, res)) {
+		    lmap_add_result(lmapd->lmap, res);
+		    valid_report = 1;
+		} else {
+		    lmap_err("failed to read data file '%s'", dp->d_name);
+		    had_errors = 1;
+
+		    lmap_result_free(res);
+		    res = NULL;
 		}
 	    }
 	    (void) close(mfd);
@@ -720,5 +732,8 @@ lmapd_workspace_read_results(struct lmapd *lmapd, const int filetype)
 	}
     }
     (void) closedir(dfd);
-    return rc;
+
+    if (had_errors && !valid_report)
+	return -1;
+    return 0;
 }