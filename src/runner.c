simetnicbr/simet-lmapd

commit d0ddc1eb76e515519d0a4e7d84bd75048b0d4412

lmapd: move action results as a group when the schedule finishes
We want to group in the same report the results of all actions of a
schedule run.  The simplest way to do this is to move the results of an
action to their destinations only when the schedule finishes running,
instead of as soon as the action finishes running.

We need to special-case actions whose destinations are their own
schedule and move the results immediately, so that (on sequential
schedules), they can still be used by the next action in the sequence.

CEPTRO.br-issue: MDC-952

src/runner.c
@@ -213,6 +213,11 @@ action_exec(struct lmapd *lmapd, struct schedule *schedule, struct action *actio
 	return -1;
     }
 
+    if (action->flags & LMAP_ACTION_FLAG_MOVEDEFERRED) {
+	lmap_wrn("action '%s' has previous results, coping with it",
+		action->name);
+    }
+
     event_base_gettimeofday_cached(lmapd->base, &t);
 
     /*
@@ -588,19 +593,30 @@ lmapd_cleanup(struct lmapd *lmapd)
 	(void) lmapd_workspace_action_meta_add_end(schedule, action);
 
 	/*
-	 * Move the results to the destinations and afterwards cleanup
-	 * the workspace.
+	 * Move the action results to the destinations and afterwards
+	 * cleanup the workspace, unless we need to defer the move to
+	 * after the whole schedule (i.e. all its actions) finished.
 	 */
 
+	/*
+	 * we only reset the MOVEDEFERRED FLAG when we move or clean,
+	 * as it is safer (less chance of data loss due to a bug).
+	 *
+	 * action->flags &= ~LMAP_ACTION_FLAG_MOVEDEFERRED;
+	 */
 	if (action->last_status == 0 && action->destinations) {
 	    for (tag = action->destinations; tag; tag = tag->next) {
-		struct schedule *dst = lmap_find_schedule(lmap, tag->tag);
-		if (dst) {
-		    (void) lmapd_workspace_action_move(lmapd, schedule, action, dst);
+		struct schedule * const dst = lmap_find_schedule(lmap, tag->tag);
+		if (dst && lmapd_workspace_action_move(lmapd, schedule, action, dst, 1) > 0) {
+		    action->flags |= LMAP_ACTION_FLAG_MOVEDEFERRED;
 		}
 	    }
 	}
-	(void) lmapd_workspace_action_clean(lmapd, action);
+	if (!(action->flags & LMAP_ACTION_FLAG_MOVEDEFERRED)) {
+	    /* cleanup early to free space early */
+	    (void) lmapd_workspace_action_clean(lmapd, action);
+	    /* action->flags &= ~LMAP_ACTION_FLAG_MOVEDEFERRED; */
+	}
 
 	/*
 	 * Is there any subsequent action in a sequential schedule?
@@ -641,6 +657,28 @@ lmapd_cleanup(struct lmapd *lmapd)
 		}
 	    }
 	    if (schedule->state != LMAP_SCHEDULE_STATE_RUNNING) {
+		/* FIXME: this will move whatever fraction of a schedule that
+		 * did run before suppresion with SCHEDULE_FLAG_STOP_RUNNING.
+		 * In that case we might want to just drop the results, instead? */
+
+		/* Move the results of any actions that had pending results,
+		 * and clean the action workspaces */
+		for (action = schedule->actions; action; action = action->next) {
+		    if (action->flags & LMAP_ACTION_FLAG_MOVEDEFERRED) {
+			for (tag = action->destinations; tag; tag = tag->next) {
+			    struct schedule * const dst = lmap_find_schedule(lmap, tag->tag);
+			    if (dst) {
+				// lmap_dbg("lmap_cleanup: processing deferral for %s::%s destination %s", schedule->name, action->name, dst->name);
+				(void) lmapd_workspace_action_move(lmapd, schedule, action, dst, 0);
+			    }
+			}
+		    }
+		    // lmap_dbg("lmap_cleanup: final cleanup for %s::%s", schedule->name, action->name);
+		    action->flags &= ~LMAP_ACTION_FLAG_MOVEDEFERRED;
+		    (void) lmapd_workspace_action_clean(lmapd, action);
+		}
+
+		/* account for the schedule run, and cleanup its input queue */
 		if (failed) {
 		    schedule->cnt_failures++;
 		} else if (succeeded) {

src/lmap.h.in
@@ -215,6 +215,7 @@ struct action {
     struct action *next;
 
     int8_t state;
+    int32_t flags;
     uint64_t storage;
     time_t last_invocation;
     time_t last_completion;
@@ -238,6 +239,8 @@ struct action {
 #define LMAP_ACTION_STATE_DISABLED		0x02
 #define LMAP_ACTION_STATE_RUNNING		0x03
 #define LMAP_ACTION_STATE_SUPPRESSED		0x04
+
+#define LMAP_ACTION_FLAG_MOVEDEFERRED		0x01
 
 extern struct action * lmap_action_new();
 extern void lmap_action_free(struct action *action);

src/workspace.c
@@ -458,16 +458,23 @@ lmapd_workspace_action_clean(struct lmapd *lmapd, struct action *action)
  * for consumption (e.g. by the next action in a sequential execution
  * mode).
  *
+ * The "defer" parameter can be used to move the results of all
+ * actions of a schedule only after the schedule finishes running,
+ * while still immediately moving the output of actions directed
+ * to their own schedule (see above).
+ *
  * @param lmapd pointer to the struct lmapd
  * @param schedule pointer to the struct schedule
  * @param action pointer to the struct action
  * @param destination pointer to the struct schedule
- * @return 0 on success, -1 on error
+ * @param defer defer moves destined to a different schedule
+ * @return 0 on success, -1 on error, 1 if the move was deferred
  */
 
 int
 lmapd_workspace_action_move(struct lmapd *lmapd, struct schedule *schedule,
-		            struct action *action, struct schedule *destination)
+		            struct action *action, struct schedule *destination,
+			    int defer)
 {
     int ret = 0;
     char oldfilepath[PATH_MAX];
@@ -484,6 +491,13 @@ lmapd_workspace_action_move(struct lmapd *lmapd, struct schedule *schedule,
 	|| !destination || !destination->workspace) {
 	return 0;
     }
+
+    /* we want to process moves to the action's own schedule
+     * immediately, but we might want to defer moves to any other
+     * schedule to happen when the schedule finishes (i.e. all
+     * actions have finished). */
+    if (defer && destination != schedule)
+	return 1;
 
     if (destination != schedule) {
 	newfileformat = "%s/" LMAPD_QUEUE_INCOMING_NAME "/%s";

src/workspace.h
@@ -26,7 +26,7 @@ extern int lmapd_workspace_clean(struct lmapd *lmapd);
 extern int lmapd_workspace_update(struct lmapd *lmapd);
 
 extern int lmapd_workspace_action_clean(struct lmapd *lmapd, struct action *action);
-extern int lmapd_workspace_action_move(struct lmapd *lmapd, struct schedule *schedule, struct action *action, struct schedule *destination);
+extern int lmapd_workspace_action_move(struct lmapd *lmapd, struct schedule *schedule, struct action *action, struct schedule *destination, int defer);
 extern int lmapd_workspace_schedule_move(struct lmapd *lmapd, struct schedule *schedule);
 extern int lmapd_workspace_schedule_clean(struct lmapd *lmapd, struct schedule *schedule);
 

commit 556c2abd3eed46080bc65dd18106be729bdc3ee0

lmapd: fix move of action results for one-off, startup and immediate
Properly process the results of actions for schedules of types
"one-off", "startup", and "immediate".  They were being incorrectly left
behind.

These schedule types have their state set to DISABLED while their
actions are still running, unlike the other schedule types.  The other
schedule types keep the RUNNING state while their actions are still
running, and switch to ENABLED when their last action exits.

Change lmap_cleanup() to track still-running state based on the state of
the schedule actions, and process the schedule cleanup when the last
action exists, regardless of schedule state.  This includes moving the
results of any actions.

Fixes: commit d0ddc1eb76e515519d0a4e7d84bd75048b0d4412
       lmapd: move action results as a group when the schedule finishes

CEPTRO.br-issue: MDC-1266

src/runner.c
@@ -533,7 +533,7 @@ void
 lmapd_cleanup(struct lmapd *lmapd)
 {
     pid_t pid;
-    int status, failed, succeeded;
+    int status, succeeded, still_running;
     struct lmap *lmap;
     struct timeval t;
     struct action *action;
@@ -594,8 +594,9 @@ lmapd_cleanup(struct lmapd *lmapd)
 
 	/*
 	 * Move the action results to the destinations and afterwards
-	 * cleanup the workspace, unless we need to defer the move to
-	 * after the whole schedule (i.e. all its actions) finished.
+	 * cleanup the action workspace, unless we need to defer the
+	 * move to after the whole schedule (i.e. all its actions)
+	 * finished.
 	 */
 
 	/*
@@ -624,7 +625,6 @@ lmapd_cleanup(struct lmapd *lmapd)
 	 * schedule got meanwhile suppressed and the stop all running
 	 * flag is set.
 	 */
-
 	if (action->next && schedule
 	    && schedule->mode == LMAP_SCHEDULE_EXEC_MODE_SEQUENTIAL) {
 	    if (schedule->state != LMAP_SCHEDULE_STATE_SUPPRESSED
@@ -634,29 +634,30 @@ lmapd_cleanup(struct lmapd *lmapd)
 	}
 
 	/*
-	 * Change schedule state back to enabled if all actions have
-	 * left the running state.  If at least one action was executed
-	 * and every action returned success, clean up the schedule
-	 * input processing queue.
+	 * RUNNING schedules need to be switched to ENABLED when all of
+	 * its actions have left the running state, and processed for
+	 * MOVEDEFERRED.
+	 *
+	 * DISABLED schedules need to be processed for MOVEDEFERRED
+	 * when all of its actions have left the running state.
+	 *
+	 * Cleanup the schedule processing queue if at least one action
+	 * was executed, and every executed action returned success.
 	 */
-
-	if (schedule->state == LMAP_SCHEDULE_STATE_RUNNING) {
+	still_running = succeeded = 0;
+	for (action = schedule->actions; action; action = action->next) {
+	    still_running |= (action->state == LMAP_ACTION_STATE_RUNNING);
+	    succeeded     |= (action->last_status == 0);
+	}
+	if (schedule->state == LMAP_SCHEDULE_STATE_RUNNING && !still_running) {
 	    schedule->state = LMAP_SCHEDULE_STATE_ENABLED;
 	    if (schedule->cnt_active_suppressions) {
 		schedule->state = LMAP_SCHEDULE_STATE_SUPPRESSED;
 	    }
-	    succeeded = failed = 0;
-	    for (action = schedule->actions; action; action = action->next) {
-		if (action->state == LMAP_ACTION_STATE_RUNNING) {
-		    schedule->state = LMAP_SCHEDULE_STATE_RUNNING;
-		}
-		if (action->last_status) {
-		    failed = 1;
-		} else {
-		    succeeded = 1;
-		}
-	    }
-	    if (schedule->state != LMAP_SCHEDULE_STATE_RUNNING) {
+	}
+
+	{
+	    if (!still_running) {
 		/* FIXME: this will move whatever fraction of a schedule that
 		 * did run before suppresion with SCHEDULE_FLAG_STOP_RUNNING.
 		 * In that case we might want to just drop the results, instead? */
@@ -679,11 +680,11 @@ lmapd_cleanup(struct lmapd *lmapd)
 		}
 
 		/* account for the schedule run, and cleanup its input queue */
-		if (failed) {
-		    schedule->cnt_failures++;
-		} else if (succeeded) {
+		if (succeeded) {
 		    /* there was at least one action, and none failed */
 		    lmapd_workspace_schedule_clean(lmapd, schedule);
+		} else {
+		    schedule->cnt_failures++;
 		}
 	    }
 	}