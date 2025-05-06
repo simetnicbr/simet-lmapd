simetnicbr/simet-lmapd

commit 1cf4bf4b2ae648564525f93dc48fddc0be2dbd65

lmapd: skip events of type startup on SIGHUP reload (v2)
Do not run events with a trigger type of 'startup' more than once, even
if a new (or the same) schedule is reloaded through SIGHUP.  This is
closer to the RFC8194 behavior (a lmapd config reload is *not* a
restart, even if internally it is handled as such).

Before this change, lmapd would treat trigger events of type 'startup'
and 'immediate' as effectively the same thing.  They would be triggered
right after starting, and also (if they were still present in the
schedule configuration) after every SIGHUP (i.e. on configuration
reload).

With this change, lmapd will trigger 'startup' events at most once per
session (a session begins when lmapd is started, and ends when it stops
/ terminates).  Once lmapd has triggered at least one LMAP action with
an event type of 'startup', it will disable that trigger *type* for any
*future* configuration reloads / SIGHUP.

Note that it *will* process all 'startup' triggers on the current
configuration, unless interrupted (by a reload, etc).

A lmapd *service* restart (which stops the lmapd process entirely, and
starts it anew) will clear state, and run 'startup' events again if
present in the schedule.

Events of type 'immediate' are unmodified, and will run on start, and
also after configuration reloads.

CEPTRO.br-issue: MDC-1466

src/runner.c
@@ -784,6 +784,9 @@ execute_cb(struct lmapd *lmapd, struct event *event)
 		|| event->type == LMAP_EVENT_TYPE_STARTUP) {
 		sched->state = LMAP_SCHEDULE_STATE_DISABLED;
 	    }
+	    if (event->type == LMAP_EVENT_TYPE_STARTUP) {
+		lmapd->flags |= LMAPD_FLAG_STARTUPDONE;
+	    }
 	}
 
     next:
@@ -1112,6 +1115,11 @@ lmapd_run(struct lmapd *lmapd)
 		break;
 
 	    case LMAP_EVENT_TYPE_STARTUP:
+		if (lmapd->flags & LMAPD_FLAG_SKIPSTARTUP) {
+		    lmap_dbg("skipping startup event '%s' on restart", event->name);
+		    break;
+		}
+		/* falltrough */
 	    case LMAP_EVENT_TYPE_IMMEDIATE:
 		add_random_spread(event, &tv);
 		event_gaga(event, &event->fire_event, EV_TIMEOUT, fire_cb, &tv);
@@ -1203,6 +1211,9 @@ void
 lmapd_restart(struct lmapd *lmapd)
 {
     lmapd->flags |= LMAPD_FLAG_RESTART;
+    if (lmapd->flags & LMAPD_FLAG_STARTUPDONE) {
+	lmapd->flags |= LMAPD_FLAG_SKIPSTARTUP;
+    }
     lmapd_killall(lmapd);
     if (event_base_loopbreak(lmapd->base) < 0) {
 	lmap_err("failed to break the event loop");

src/lmapd.h.in
@@ -58,6 +58,8 @@ struct lmapd {
 };
 
 #define LMAPD_FLAG_RESTART	0x01
+#define LMAPD_FLAG_SKIPSTARTUP	0x02
+#define LMAPD_FLAG_STARTUPDONE	0x04
 
 extern struct lmapd * lmapd_new();
 extern void lmapd_free(struct lmapd *lmapd);

commit dcca568524b95d117baa5b180ce79603564198bb

lmapd: fix typo in fallthrough comment
gcc can actually use these comments to avoid issuing a warning (useful
since the lmapd code is not using the C23 [[fallthrough]] annotation),
so it deserves a fix.

Fixes: commit 1cf4bf4b2ae648564525f93dc48fddc0be2dbd65
       (lmapd: skip events of type startup on SIGHUP reload (v2))

CEPTRO.br-issue: MDC-1466

src/runner.c
@@ -1119,7 +1119,7 @@ lmapd_run(struct lmapd *lmapd)
 		    lmap_dbg("skipping startup event '%s' on restart", event->name);
 		    break;
 		}
-		/* falltrough */
+		/* fallthrough */
 	    case LMAP_EVENT_TYPE_IMMEDIATE:
 		add_random_spread(event, &tv);
 		event_gaga(event, &event->fire_event, EV_TIMEOUT, fire_cb, &tv);